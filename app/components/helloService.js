f1b35b3e7dd00ec087ea640119f040b0220679e3
Artokun <art.longbottom.jr@gmail.com>
2015-03-31 17:42:46 -0700

    step 1

---
+++ app/components/helloService.js
@@ -0,0 +1,12 @@
+/*globals angluar*/
+angular.module('HelloModule', [])
+  .factory('uppercaseService', function () {
+    return function () {
+      return 'hello';
+    }
+  })
+  .factory('helloService', function (uppercaseService) {
+    return function () {
+      return uppercaseService('hello');
+    }
+  });
